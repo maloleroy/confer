9f2f5a04401c929a5516ed179cebbe166693a4e6
fluteur <malo.leroy@student-cs.fr>
2025-04-08 02:11:14 +0200

    🚑️ Fix confer.h

--- include/confer.h
+++ include/confer.h
@@ -5,10 +5,10 @@
 #include "build.h"
 #include "print.h"
 
-#define cfInit init_root_scope
+#define cf_init init_root_scope
 
 #define CFTEST struct cfScope *self
-#define cfTest(f)                   \
+#define cf_test(f)                   \
     static struct cfScope node_##f; \
     init_scope(node_##f);            \
     _cf_add_child(self, &node_##f);   \
